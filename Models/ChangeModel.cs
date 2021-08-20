30fd63cecf07dda1372d26582c29c05169d392de
drt0927 <drt_0927@naver.com>
2021-08-21 02:00:51 +0900

    기타 기능 추가
    1. 아바타 기능 추가
    2. Changes 상태값별로 색상 추가

--- Models/ChangeModel.cs
+++ Models/ChangeModel.cs
@@ -5,13 +5,101 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace gitup.Models
 {
 	public class ChangeModel
 	{
 		public ChangeKind? Status { get; set; }
+		public string StatusString
+		{
+			get
+			{
+				switch (Status)
+				{
+					case ChangeKind.Deleted:
+						return "Delete";
+					case ChangeKind.Renamed:
+					case ChangeKind.TypeChanged:
+					case ChangeKind.Modified:
+						return "Modify";
+					case ChangeKind.Added:
+						return "Add";
+					default:
+						return Status.ToString();
+				}
+			}
+		}
+		public Brush StatusColor
+		{
+			get
+			{
+				switch (Status)
+				{
+					case ChangeKind.Deleted:
+						return (Brush)(new BrushConverter().ConvertFrom("#D73A49"));
+					case ChangeKind.Renamed:
+					case ChangeKind.TypeChanged:
+					case ChangeKind.Modified:
+						return (Brush)(new BrushConverter().ConvertFrom("#DBAB09"));
+					case ChangeKind.Added:
+						return (Brush)(new BrushConverter().ConvertFrom("#29A045"));
+					default:
+						return Brushes.White;
+				}
+			}
+		}
 		public FileStatus? State { get; set; }
+		public string StateString
+		{
+			get
+			{
+				switch (State)
+				{
+					case FileStatus.DeletedFromIndex:
+					case FileStatus.DeletedFromWorkdir:
+						return "Delete";
+					case FileStatus.RenamedInIndex:
+					case FileStatus.RenamedInWorkdir:
+					case FileStatus.TypeChangeInIndex:
+					case FileStatus.TypeChangeInWorkdir:
+					case FileStatus.ModifiedInIndex:
+					case FileStatus.ModifiedInWorkdir:
+						return "Modify";
+					case FileStatus.NewInIndex:
+					case FileStatus.NewInWorkdir:
+						return "Add";
+					default:
+						return State.ToString();
+				}
+			}
+		}
+		public Brush StateColor
+		{
+			get
+			{
+				switch (State)
+				{
+					case FileStatus.DeletedFromIndex:
+					case FileStatus.DeletedFromWorkdir:
+						return (Brush)(new BrushConverter().ConvertFrom("#D73A49"));
+					case FileStatus.RenamedInIndex:
+					case FileStatus.RenamedInWorkdir:
+					case FileStatus.TypeChangeInIndex:
+					case FileStatus.TypeChangeInWorkdir:
+					case FileStatus.ModifiedInIndex:
+					case FileStatus.ModifiedInWorkdir:
+						return (Brush)(new BrushConverter().ConvertFrom("#DBAB09"));
+					case FileStatus.NewInIndex:
+					case FileStatus.NewInWorkdir:
+						return (Brush)(new BrushConverter().ConvertFrom("#29A045"));
+					default:
+						return Brushes.White;
+				}
+			}
+		}
+
 		public string Path { get; set; }
 		public string Name { get; set; }
 
--- Models/CommitModel.cs
+++ Models/CommitModel.cs
@@ -1,13 +1,16 @@
-﻿using gitup.Extensions;
+﻿using Devcorner.NIdenticon;
+using Devcorner.NIdenticon.BrushGenerators;
+using gitup.Extensions;
 using LibGit2Sharp;
 using Prism.Commands;
 using System;
-using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media.Imaging;
 
 namespace gitup.Models
 {
@@ -21,6 +24,7 @@ namespace gitup.Models
 		public DateTimeOffset Date { get; set; }
 		public string Branches { get; set; }
 		public ICommand OpenChangesCommand { get; set; }
+		public BitmapImage Avatar { get; set; }
 		private string _path { get; set; }
 		public CommitModel(Commit commit, string path)
 		{
@@ -32,6 +36,39 @@ namespace gitup.Models
 			this.Date = commit.Author.When;
 			this.ParentShortId = commit.Parents.FirstOrDefault()?.ShortSha();
 			OpenChangesCommand = new DelegateCommand(OpenChangesWindow);
+
+			using (MemoryStream ms = new MemoryStream())
+			{
+				// 문자열 HEX로 변환
+				string resultHex = string.Empty;
+				byte[] arr_byteStr = Encoding.Default.GetBytes(this.Author);
+
+				foreach (byte byteStr in arr_byteStr)
+					resultHex += string.Format("{0:X2}", byteStr);
+
+				// HEX값 Color로 변환
+				var color = System.Drawing.ColorTranslator.FromHtml($"#{(resultHex.Length > 6 ? resultHex.Substring(resultHex.Length - 6) : resultHex)}");
+
+				// 단색 Brush 생성
+				IBrushGenerator brushGenerator = new StaticColorBrushGenerator(color);
+
+				// Avatar 생성
+				var ig = new IdenticonGenerator()
+					.WithSize(25, 25)
+					.WithBlocks(6, 4)
+					.WithAlgorithm("MD5")
+					.WithBackgroundColor(Color.White)
+					.WithBrushGenerator(brushGenerator);
+				var bitmap = ig.Create(this.Author);
+				bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+
+				// Image 생성
+				this.Avatar = new BitmapImage();
+				this.Avatar.BeginInit();
+				this.Avatar.StreamSource = ms;
+				this.Avatar.CacheOption = BitmapCacheOption.OnLoad;
+				this.Avatar.EndInit();
+			}
 		}
 
 		public void OpenChangesWindow()
